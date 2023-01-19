4981dcb7b22d75ff91d9d9d8793d066f856faf2a
SA6MWA Michel <sa6mwa@gmail.com>
2023-01-19 22:21:57 +0100

    Stash

--- cmd/hfprop/main.go
+++ cmd/hfprop/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/sa6mwa/hfprop"
@@ -8,6 +10,13 @@ import (
 
 func main() {
 
-	hfprop.GetGiroData("foF2", "JR055", time.Now().Add(-1*time.Hour), time.Now())
+	hfprop.SetDistanceForMUF(100.0)
+	gd, err := hfprop.GetGiroData("foF2", "JR055", time.Now().Add(-1*time.Hour), time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	if len(gd) != 0 {
+		fmt.Println(gd[0].Parameter, "=", gd[0].Value)
+	}
 }
--- hfprop.go
+++ hfprop.go
@@ -1,35 +1,55 @@
 package hfprop
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
+// FastChar: https://giro.uml.edu/didbase/scaled.php
 var (
+	DMUF              string = DefaultDMUF
+	DefaultDMUF       string = "3000"
 	LgdcBaseUrl       string = "https://lgdc.uml.edu/common/DIDBGetValues"
 	LgdcKeyUrsiCode   string = "ursiCode"
-	LgdcKeyCharName   string = "charName"
+	LgdcKeyCharName   string = "charName" // char = characteristics
 	LgdcKeyDMUF       string = "DMUF"
 	LgdcKeyFromDate   string = "fromDate"
 	LgdcKeyToDate     string = "toDate"
-	DefaultUrsiCode   string = "JR055"
-	DefaultDMUF       string = "3000"
 	GiroTimeFormatIn  string = "2006-01-02 15:04:05"
 	GiroTimeFormatOut string = "2006-01-02T15:04:05.000Z"
+	DefaultUrsiCode   string = "JR055"
 )
 
-func GetGiroData(parameter string, ursiCode string, from time.Time, to time.Time) error {
+// Configure distance for MUF (Maximum Usable Frequency (D)) when requesting the MUFD parameter
+func SetDistanceForMUF(km float64) {
+	DMUF = fmt.Sprintf("%.0f", km)
+}
 
-	// 2023-01-18T19:08:16.000Z
+type GiroData struct {
+	Time      time.Time
+	Parameter string
+	Value     float64
+}
 
+// GetGiroData retrieves data for a single characteristic (parameter) from the
+// DIDB at lgdc.uml.edu between from time and to time. Returns a slice of
+// GiroData objects or error if there was an error.
+//
+// Retrieve foF2 from Juliusruh (JR055) for the last hour. The gd slice is
+// reversed meaning the latest value is the first entry in the slice.
+//
+//	gd, err := hfprop.GetGiroData("foF2", "JR055", time.Now().Add(-1*time.Hour), time.Now())
+func GetGiroData(parameter string, ursiCode string, from time.Time, to time.Time) ([]GiroData, error) {
+	gds := make([]GiroData, 0, 20)
 	u, err := url.Parse(LgdcBaseUrl)
 	if err != nil {
-		return err
+		return gds, err
 	}
-
 	values := url.Values{
 		LgdcKeyUrsiCode: {ursiCode},
 		LgdcKeyCharName: {parameter},
@@ -37,21 +57,54 @@ func GetGiroData(parameter string, ursiCode string, from time.Time, to time.Time
 		LgdcKeyFromDate: {from.UTC().Format(GiroTimeFormatIn)},
 		LgdcKeyToDate:   {to.UTC().Format(GiroTimeFormatIn)},
 	}
-
 	u.RawQuery = values.Encode()
-
-	fmt.Println(u.String())
-
 	resp, err := http.Get(u.String())
 	if err != nil {
-		return err
+		return gds, err
 	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	return gds, err
+	// }
+	s := bufio.NewScanner(resp.Body)
+	for s.Scan() {
+		if strings.HasPrefix(s.Text(), "#") || strings.TrimSpace(s.Text()) == "" {
+			continue
+		}
+		if strings.HasPrefix(s.Text(), "ERROR: ") {
+			_, str, _ := strings.Cut(s.Text(), "ERROR: ")
+			return gds, errors.New(strings.TrimSpace(str))
+		}
+		fields := strings.Fields(s.Text())
+		if len(fields) < 3 {
+			continue
+		}
+		gd := GiroData{}
+		gd.Time, err = time.Parse(GiroTimeFormatOut, fields[0])
+		if err != nil {
+			return gds, err
+		}
+		gd.Parameter = parameter
+		_, err = fmt.Sscanf(fields[2], "%f", &gd.Value)
+		if err != nil {
+			return gds, err
+		}
+		gds = append(gds, gd)
 	}
+	ReverseGiroData(gds)
+	return gds, nil
+}
 
-	fmt.Print(string(body))
-	return nil
+// ReverseGiroData reverses a GiroData slice.
+func ReverseGiroData(s []GiroData) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+// ReverseStrings reverses a slice of strings.
+func ReverseStrings(s []string) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
 }
